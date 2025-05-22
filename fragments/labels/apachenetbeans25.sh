ce92995d24b519a525c4de12de1acbcb7884e85e
John Bowman <jcmbowman@gmail.com>
2025-05-22 14:40:25 -0400

    Create apachenetbeans25.sh
    Apache Netbeans v26 is currently without a .pkg deployment. Creating a label that locks to version 25

---
+++ fragments/labels/apachenetbeans25.sh
@@ -0,0 +1,7 @@
+apachenetbeans25)
+    name="Apache NetBeans"
+    type="pkg"
+    appNewVersion=25
+    downloadURL="https://dlcdn.apache.org/netbeans/netbeans-installers/"${appNewVersion}"/Apache-NetBeans-"${appNewVersion}".pkg"
+    expectedTeamID="2GLGAFWEQD"
+    ;;
