110511e29b3cf21576893aef3f41c035e0ff0a3e
John Bowman <jcmbowman@gmail.com>
2025-05-23 09:06:41 -0400

    Update mobirise.sh
    Update label to accommodate changes to the Vendor's website and the new TeamID

--- fragments/labels/mobirise.sh
+++ fragments/labels/mobirise.sh
@@ -1,6 +1,7 @@
 mobirise)
-    name="mobirise"
+    name="Mobirise"
     type="dmg"
     downloadURL="https://download.mobirise.com/MobiriseSetup.dmg"
-    expectedTeamID="UD4494E573"
+    appNewVersion=$(curl -fsL 'https://history.mobirisesite.com' | grep -A 1 "Download Mobirise " | tail -n 1 | sed -E 's/.*Mobirise ([0-9.]*)<.*/\1/g')
+    expectedTeamID="V324ZB3P82"
     ;;
