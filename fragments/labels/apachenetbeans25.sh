a72d39d85fe70c6f1d8652a37edc6e7d5b7c8211
John Bowman <jcmbowman@gmail.com>
2025-07-15 09:51:48 -0400

    Fix typo in downloadURL

--- fragments/labels/apachenetbeans25.sh
+++ fragments/labels/apachenetbeans25.sh
@@ -2,6 +2,6 @@ apachenetbeans25)
     name="Apache NetBeans"
     type="pkg"
     appNewVersion=25
-    downloadURL="https://dlcdn.apache.org/netbeans/netbeans-installers/"${appNewVersion}"/Apache-NetBeans-"${appNewVersion}".pkg"
+    downloadURL="https://dlcdn.apache.org/netbeans/netbeans-installers/${appNewVersion}/Apache-NetBeans-${appNewVersion}.pkg"
     expectedTeamID="2GLGAFWEQD"
     ;;
