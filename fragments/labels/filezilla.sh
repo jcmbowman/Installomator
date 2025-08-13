5386f4e13ae9d7079f98fae3f996b27f58443ad2
John Bowman <jcmbowman@gmail.com>
2025-08-13 14:10:44 -0400

    Update filezilla.sh

--- fragments/labels/filezilla.sh
+++ fragments/labels/filezilla.sh
@@ -4,12 +4,13 @@ filezilla)
     packageID="org.filezilla-project.filezilla"
     if [[ $(arch) == "arm64" ]]; then
         cpu_arch="arm64"
+        url_platform="macos-arm64"
     elif [[ $(arch) == "i386" ]]; then
         cpu_arch="x86"
+        url_platform="osx"
     fi
-    downloadURL=$(curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15" -fsL https://filezilla-project.org/download.php\?show_all=1 | grep macos-$cpu_arch | head -n 1 | awk -F '"' '{print $2}' )
-    appNewVersion=$( curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15" -fsL https://filezilla-project.org/download.php\?show_all=1 | grep macos-$cpu_arch | head -n 1 | awk -F '_' '{print $2}' )
+    appNewVersion=$( curl -A "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15" -fsL "https://filezilla-project.org/download.php?platform=$url_platform" | grep 'name="description"' | head -n 1 | awk -F ' ' '{print $6}' )
+    downloadURL="https://dl4.cdn.filezilla-project.org/client/FileZilla_${appNewVersion}_macos-${cpu_arch}.app.tar.bz2"
     expectedTeamID="5VPGKXL75N"
     blockingProcesses=( NONE )
     ;;
-    
